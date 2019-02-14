f8ac7c80ecbe1e9270fc344625303363a21d8321
beijinghuakang <genius-chao@163.com>
2019-02-14 13:02:13 +0800

    分包机

---
+++ src/main/java/com/hkc/project/standard/ReadManuInfo.java
@@ -0,0 +1,12 @@
+package com.hkc.project.standard;
+
+/**
+ * @author csc
+ * @create 2019-02-14 12:59
+ */
+public class ReadManuInfo implements  Runnable {
+    @Override
+    public void run() {
+
+    }
+}

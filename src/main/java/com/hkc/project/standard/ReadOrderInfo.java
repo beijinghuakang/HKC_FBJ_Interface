5cc923e62ac528aed4a1fc394e2d54f6e4f65ae5
beijinghuakang <genius-chao@163.com>
2019-02-14 12:57:28 +0800

    分包机

---
+++ src/main/java/com/hkc/project/standard/ReadOrderInfo.java
@@ -0,0 +1,12 @@
+package com.hkc.project.standard;
+
+/**
+ * @author csc
+ * @create 2019-02-14 12:56
+ */
+public class ReadOrderInfo implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
